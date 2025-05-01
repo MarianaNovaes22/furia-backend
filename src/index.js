777d78ddbd1c03c330022745598eb1fc40f14114
Mariana <mariana.c.novaes22@gmail.com>
2025-05-01 01:51:45 -0300

    Corrigindo endpoint e configurando deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://furia-chatbot-chi.vercel.app"],
 }))
 app.use(express.json());
 
