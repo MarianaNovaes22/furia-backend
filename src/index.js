35e7cc123a6edb0e8bc121d344c89355ae50a28b
Mariana <mariana.c.novaes22@gmail.com>
2025-05-01 01:41:47 -0300

    Terceiro commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import chatRoutes from './routes/chatRoutes.js';
 
-
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+}))
 app.use(express.json());
 
 app.use('/chat', chatRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
